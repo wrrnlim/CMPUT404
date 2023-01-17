c1d6b753e3735f2d8b91685c73d7a33aae9d1ae6
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2023-01-16 22:24:33 -0700

    complete lab2

--- lab2/echo_server.py
+++ lab2/echo_server.py
@@ -1,6 +1,7 @@
 # Connect to this server using echo "foobar" | nc localhost 8080 -q 1
 
 import socket
+from threading import Thread
 
 BYTES_TO_READ = 4096
 HOST = "127.0.0.1"
@@ -29,4 +30,15 @@ def start_server():
 
   return
 
-start_server()
+def start_threaded_server():
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # allows socket to reuse a connection
+    s.listen(2)
+    while True:
+      conn, addr = s.accept()
+      thread = Thread(target=handle_connection, args=(conn, addr))
+      thread.run()
+
+# start_server()
+start_threaded_server()
